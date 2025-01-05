efc8b503e9f731bcf78cc97e71dace57f7067d07
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-01-05 19:13:43 +0000

    use query client provider

--- ts/packages/frontend/src/pages/_app.tsx
+++ ts/packages/frontend/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@frontend/styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  })
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
