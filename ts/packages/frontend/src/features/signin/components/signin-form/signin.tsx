fb896d540eaa43c72de20dafe9c421cd1029f9e8
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-01-05 17:43:48 +0000

    add logo

--- ts/packages/frontend/src/features/signin/components/signin-form/signin.tsx
+++ ts/packages/frontend/src/features/signin/components/signin-form/signin.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import { SlackSSOButton } from '../slack-sso-button'
 
 export function SigninForm() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="flex w-full max-w-md flex-col items-center gap-16">
+        <Image
+          src="/images/logo.jpg"
+          alt="Stack Example"
+          width={120}
+          height={120}
+        />
         <SlackSSOButton />
       </div>
     </div>
