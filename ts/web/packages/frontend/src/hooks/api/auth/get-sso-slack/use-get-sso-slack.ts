c42cfb09c043c317a6c1b2bd53369e23ccd7a825
Takuya Ashikawa <65634668+t-ash0410@users.noreply.github.com>
2025-01-07 05:02:13 +0900

    impl suppress query call (#25)

--- ts/web/packages/frontend/src/hooks/api/auth/get-sso-slack/use-get-sso-slack.ts
+++ ts/web/packages/frontend/src/hooks/api/auth/get-sso-slack/use-get-sso-slack.ts
@@ -4,7 +4,10 @@ import { useQuery } from '@tanstack/react-query'
 import { authQueryKeys } from '../query-key-factory'
 import type { GetSSOSlackQueryParams } from './types'
 
-export const useGetSSOSlack = (query: GetSSOSlackQueryParams) => {
+export const useGetSSOSlack = (
+  query: GetSSOSlackQueryParams,
+  enabled: boolean,
+) => {
   const bff = useBff()
   return useQuery({
     queryKey: [...authQueryKeys.ssoSlack(query)],
@@ -15,5 +18,6 @@ export const useGetSSOSlack = (query: GetSSOSlackQueryParams) => {
       if (!res.ok) handleError('システムエラー')
       return await res.json()
     },
+    enabled,
   })
 }
