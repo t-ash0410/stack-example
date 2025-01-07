3aec732bcc3d498b8c8bf325fe7a40d35afc8b8f
Takuya Ashikawa <65634668+t-ash0410@users.noreply.github.com>
2025-01-08 01:41:33 +0900

    impl crate ticket api (#29)

---
+++ ts/web/packages/bff/src/routes/tickets/create.ts
@@ -0,0 +1,46 @@
+import type { AuthNEnv, ValidatorInput } from '@bff/types'
+import { timestampFromDate } from '@bufbuild/protobuf/wkt'
+import { zValidator } from '@hono/zod-validator'
+import type { Context } from 'hono'
+import { ResultAsync } from 'neverthrow'
+import { z } from 'zod'
+
+const validator = zValidator(
+  'json',
+  z.object({
+    title: z.string(),
+    description: z.string(),
+    deadline: z.string().transform((s) => new Date(s)),
+  }),
+)
+
+type CreateTicketContext = Context<
+  AuthNEnv,
+  '',
+  ValidatorInput<typeof validator>
+>
+
+const handler = async (c: CreateTicketContext) => {
+  const res = await createTicket(c)
+  if (res.isErr()) {
+    throw res.error
+  }
+  return c.json({
+    ticketId: res.value.ticketId,
+  })
+}
+
+const createTicket = (ctx: CreateTicketContext) => {
+  const { activeUser, ticketMgrServiceClient } = ctx.var
+  const { title, description, deadline } = ctx.req.valid('json')
+  return ResultAsync.fromThrowable(() =>
+    ticketMgrServiceClient.createTicket({
+      requestedBy: activeUser.userId,
+      title,
+      description,
+      deadline: timestampFromDate(deadline),
+    }),
+  )()
+}
+
+export { handler as createHandler, validator as createValidator }
