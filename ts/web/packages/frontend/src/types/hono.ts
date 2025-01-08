cf677e45d9a35e6b5c2fca331e2ae037476a2f87
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-01-08 23:13:06 +0000

    fix utility types

--- ts/web/packages/frontend/src/types/hono.ts
+++ ts/web/packages/frontend/src/types/hono.ts
@@ -1,5 +1,5 @@
 import type { App } from '@stack-example/bff'
-import type { ClientRequestOptions, ClientResponse, hc } from 'hono/client'
+import type { ClientResponse, hc } from 'hono/client'
 
 type BFF = ReturnType<typeof hc<App>>
 
@@ -7,24 +7,25 @@ type BFF = ReturnType<typeof hc<App>>
 type BFFResponse<T extends (...args: any[]) => Promise<ClientResponse<any>>> =
   Awaited<ReturnType<T>> extends ClientResponse<infer Out> ? Out : never
 
+type BFFRequestArgs = {
+  // biome-ignore lint/suspicious/noExplicitAny: Defined as a utility type, it should be widely available.
+  query: any
+  // biome-ignore lint/suspicious/noExplicitAny: Defined as a utility type, it should be widely available.
+  param: any
+  // biome-ignore lint/suspicious/noExplicitAny: Defined as a utility type, it should be widely available.
+  json: any
+}
+
 type BFFRequestQueryParams<
   T extends (
-    args: {
-      // biome-ignore lint/suspicious/noExplicitAny: Defined as a utility type, it should be widely available.
-      query: any
-    },
-    options?: ClientRequestOptions,
+    args: BFFRequestArgs,
     // biome-ignore lint/suspicious/noExplicitAny: Defined as a utility type, it should be widely available.
   ) => any,
 > = Parameters<T>['0']['query']
 
 type BFFRequestJsonParams<
   T extends (
-    args: {
-      // biome-ignore lint/suspicious/noExplicitAny: Defined as a utility type, it should be widely available.
-      json: any
-    },
-    options?: ClientRequestOptions,
+    args: BFFRequestArgs,
     // biome-ignore lint/suspicious/noExplicitAny: Defined as a utility type, it should be widely available.
   ) => any,
 > = Parameters<T>['0']['json']
