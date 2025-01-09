17e1cea7845e77750c40a181f66574f465797e48
Takuya Ashikawa <65634668+t-ash0410@users.noreply.github.com>
2025-01-09 11:48:48 +0900

    impl delete session (#38)

--- ts/web/packages/bff/src/routes/session/route.test.ts
+++ ts/web/packages/bff/src/routes/session/route.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'bun:test'
+import { JWT_KEY } from '@bff/env'
 import { activeUser, initHonoApp } from '@bff/testutil'
 import { sessionRoute } from './route'
 
@@ -12,5 +13,20 @@ describe('GET /', () => {
     expect(await res.json()).toEqual({
       userId: activeUser.userId,
     })
+  })
+})
+
+describe('DELETE /', () => {
+  it('returns 200 response', async () => {
+    const app = initHonoApp().route('', sessionRoute)
+
+    const res = await app.request('/', {
+      method: 'DELETE',
+    })
+
+    expect(res.status).toBe(200)
+    expect(res.headers.get('set-cookie')).toBe(
+      `${JWT_KEY}=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict`,
+    )
   })
 })
