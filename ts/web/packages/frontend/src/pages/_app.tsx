3aee2fe2a967c8ffffad45baf2deff51340106a9
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-01-09 03:10:33 +0000

    apply _app.tsx

--- ts/web/packages/frontend/src/pages/_app.tsx
+++ ts/web/packages/frontend/src/pages/_app.tsx
@@ -1,16 +1,11 @@
+import { getQueryClient } from '@frontend/lib'
 import '@frontend/styles/globals.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'sonner'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  })
+  const queryClient = getQueryClient()
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
