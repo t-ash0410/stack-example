e303b44e419f1bae4ac3da493c2577c498c0c636
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-01-09 02:00:15 +0000

    impl layout

---
+++ ts/web/packages/frontend/src/pages/_layout.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import type { ReactNode } from 'react'
+
+export const Layout = (props: {
+  title: string
+  children: ReactNode
+}) => {
+  const t = `${props.title} | Stack Example`
+  return (
+    <>
+      <Head>
+        <title>{t}</title>
+      </Head>
+      {props.children}
+      <footer>
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="text-sm text-left">© 2025 Stack Example</p>
+        </div>
+      </footer>
+    </>
+  )
+}
